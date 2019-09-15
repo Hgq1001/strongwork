7180bee582d29154c1b5405acbdbe3fd1128f6a5
Hgq1001 <1049177554@qq.com>
2019-09-15 16:13:38 +0800

    update

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -1,16 +1,18 @@
 /**
  * @Author:hgq
  * @Date: 2019/9/7
- * @Describe: 首页
+ * @Describe: 联系我们
  */
 import React from 'react';
 import styles from './ContactUs.css';
+// 106.601802,29.699524
 
 const ContactUs = () => {
   return (
     <div className={'container'}>
       <img src={require('../../assets/images/contactUs/banner.jpg')} height={'650px'} width={'100%'} alt=""/>
       <img src={require('../../assets/images/contactUs/contact.jpg')} height={'100%'} width={'100%'} alt=""/>
+      {/*<img src="https://restapi.amap.com/v3/staticmap?location=116.481485,39.990464&zoom=10&size=750*300&markers=mid,,A:116.481485,39.990464&key=10e14f7c68d20a8c7993db374f42854e" alt=""/>*/}
     </div>
   );
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,12 +5,13 @@
  */
 import React from 'react';
 import styles from './Home.css';
+import router from 'umi/router';
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <img src={require('../../assets/images/home/banner.jpg')} height={'100%'} width={'100%'} alt=""/>
-      <img src={require('../../assets/images/home/about.jpg')} height={'100%'} width={'100%'} alt=""/>
+      <img src={require('../../assets/images/home/about.jpg')} style={{ cursor: 'pointer'}} onClick={()=>router.push('/aboutUs')} height={'100%'} width={'100%'} alt=""/>
       <img src={require('../../assets/images/home/partner.jpg')} height={'100%'} width={'100%'} alt=""/>
     </div>
   );
